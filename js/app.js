8a3d38fc947062713d6e4372c978f555428306fc
Jaime Pillora <jpillora@gmail.com>
2013-07-29 11:46:46 +1000

    updated

--- js/app.js
+++ js/app.js
@@ -1,4 +1,27 @@
 (function() {
   console.log('run');
 
+  $(function() {
+    var baseUrl, update;
+    baseUrl = 'https://twitter.com/intent/tweet?';
+    update = function() {
+      var code, data, link;
+      data = {
+        hashtags: $('#hashtags').val(),
+        original_referer: 'http://github.com/',
+        text: $('#text').val(),
+        tw_p: 'tweetbutton',
+        url: $('#url').val()
+      };
+      console.log(data);
+      link = "" + baseUrl + ($.param(data));
+      code = "<a href=\"" + link + "\">\n  <img src=\"http://jpillora.com/github-twitter-button/img/tweet.png\"></img>\n</a>";
+      $('pre.code').text(code);
+      return $('.preview').html(code);
+    };
+    $(document).on('keyup', 'input', update);
+    $(document).on('click', 'button', update);
+    return update();
+  });
+
 }).call(this);
--- src/scripts/run.coffee
+++ src/scripts/run.coffee
@@ -1,2 +1,30 @@
 
 console.log 'run'
+
+$ ->
+
+  baseUrl = 'https://twitter.com/intent/tweet?'
+  update = ->
+    data =
+      hashtags: $('#hashtags').val()
+      original_referer: 'http://github.com/'
+      text: $('#text').val()
+      tw_p: 'tweetbutton'
+      url: $('#url').val()
+
+    console.log data
+
+    link = "#{baseUrl}#{$.param data}"
+
+    code = """
+      <a href="#{link}">
+        <img src="http://jpillora.com/github-twitter-button/img/tweet.png"></img>
+      </a>
+    """
+
+    $('pre.code').text code
+    $('.preview').html code
+
+  $(document).on 'keyup', 'input', update
+  $(document).on 'click', 'button', update
+  update()
